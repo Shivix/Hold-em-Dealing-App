82db347fa31983e908d157c24868cd5e14ea98ba
Mark Oborne <52997365+Shivix@users.noreply.github.com>
2020-05-30 05:13:23 +0100

    Changed enum to enum class
    changed phase enum to a more modern enum class which will avoid errenous implicit conversions

--- deck.cpp
+++ deck.cpp
@@ -1,24 +1,33 @@
 #include <random>
 #include <wx/msgdlg.h>
+#include <chrono>
 #include "deck.hpp"
 
-deck::card deck::drawnCard; // static member must be defined outside of the class
-deck::card deck::drawnCard2;
+    deck::deck() = default;
 
-deck::deck() = default;
+    deck::~deck() = default;
 
-deck::~deck() = default;
-
-void deck::deal(){
+    void deck::deal(){
 
     if(!deckEmpty){
-        if (deckCounter < 52){
-            drawnCard = deckVec[deckCounter++];
-            drawnCard2 = deckVec[deckCounter++];
+        if(deckCounter < 52){
+            if(phase == phase::preflop){
+                drawnCard = deckVec[deckCounter++];
+                drawnCard2 = deckVec[deckCounter++];
+            }
+            else if(phase == phase::flop){
+                drawnCard = deckVec[deckCounter++];
+                drawnCard2 = deckVec[deckCounter++];
+                drawnCard3 = deckVec[deckCounter++];
+            }
+            else{ // turn and river are dealt the same so can be handled in same if statement
+                drawnCard = deckVec[deckCounter++];
+            }
         }
         else{
             drawnCard = nullptr; // stops an image from being shown when the deck is empty
             drawnCard2 = nullptr;
+            drawnCard3 = nullptr;
             deckEmpty = true;
             wxMessageBox("The deck is empty, please press refill to refill the deck", "Deck Empty");
         }
@@ -29,9 +38,7 @@ void deck::deal(){
 }
 
 void deck::shuffle(){
-    std::random_device rd; // random number generator for shuffle
-    std::mt19937 rn(rd()); // function since random_shuffle was removed in c++17
-    std::shuffle(deckVec.begin(), deckVec.end(), rn); // shuffles the array of cards in a pseudo-random order
+    std::shuffle(deckVec.begin(), deckVec.end(), std::default_random_engine(std::chrono::system_clock::now().time_since_epoch().count())); // shuffles the array of cards in a pseudo-random order
 }
 
 void deck::refill(){ // "refills" the deck by setting the counter that loops through the deckVec array to 0
